05eab165b4a1fa0546acf8a5ad11448a7a273812
Aneeshnair007 <a.aneeshnair@gmail.com>
2020-05-19 17:12:42 +0000

    Lab 4

--- main.js
+++ main.js
@@ -31,7 +31,28 @@ const IPCIDR = require('ip-cidr');
  * @return {string} (Ipv6Address1) - An Ipv6Address1 address.
   * @return {object} - An object containing both an IPv4 address an its mapped ipv6 address.
  */
-function getFirstIpAddress(cidrStr, callback) {
+
+/**
+ * Calculates an IPv4-mapped IPv6 address.
+ * @param {string} ipv4 - An IPv4 address in dotted-quad format.
+ * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
+ */
+
+
+
+
+
+
+class IpAddress {
+  constructor() {
+    // IAP's global log object is used to output errors, warnings, and other
+    // information to the console, IAP's log files, or a Syslog server.
+    // For more information, consult the Log Class guide on the Itential
+    // Developer Hub https://developer.itential.io/ located
+    // under Documentation -> Developer Guides -> Log Class Guide
+    log.info('Starting the IpAddress product.');
+  }
+   getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
   let firstIpAddress = null;
@@ -77,25 +98,6 @@ function getFirstIpAddress(cidrStr, callback) {
 
 }
 
-/**
- * Calculates an IPv4-mapped IPv6 address.
- * @param {string} ipv4 - An IPv4 address in dotted-quad format.
- * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
- */
-
-
-
-
-
-
-class IpAddress {
-  constructor() {
-    // IAP's global log object is used to output errors, warnings, and other
-    // information to the console, IAP's log files, or a Syslog server.
-    // For more information, consult the Log Class guide on the Itential
-    // Developer Hub https://developer.itential.io/ located
-    // under Documentation -> Developer Guides -> Log Class Guide
-    log.info('Starting the IpAddress product.');
-  }
 }
+module.exports = new IpAddress;
 
